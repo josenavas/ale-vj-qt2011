a732a9b47c67ecfd19a2b7fb4b2fc2a8cfe57bf7
josenavasmolina@gmail.com <josenavasmolina@gmail.com@55d2aa0d-96bd-598c-80cb-e06a2779e390>
2011-12-03 21:33:41 +0000

    Resources config file addded

--- trunk/game/include/Game.h
+++ trunk/game/include/Game.h
@@ -1,7 +1,13 @@
+#include <OgreRoot.h>
+
 class Game
 {
 public:
 	Game(void);
 	virtual ~Game(void);
 	bool go(void);
+private:
+	Ogre::Root* mRoot;
+	Ogre::String mPluginsCfg;
+	Ogre::String mResourcesCfg;
 };
