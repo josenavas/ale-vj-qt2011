8bca9fa18f3caac4f924dde0332c47b676c27cb5
josenavasmolina@gmail.com <josenavasmolina@gmail.com@55d2aa0d-96bd-598c-80cb-e06a2779e390>
2011-12-04 09:53:07 +0000

    Testing ogre installation

--- trunk/game/include/Game.h
+++ trunk/game/include/Game.h
@@ -1,13 +1,32 @@
 #include <OgreRoot.h>
+#include <OISEvents.h>
+#include <OISInputManager.h>
+#include <OISKeyboard.h>
+#include <OISMouse.h>
+#include <OgreWindowEventUtilities.h>
 
-class Game
+class Game : public Ogre::WindowEventListener, public Ogre::FrameListener
 {
 public:
 	Game(void);
 	virtual ~Game(void);
 	bool go(void);
+
+protected:
+	virtual void windowResized(Ogre::RenderWindow* rw);
+	virtual void windowClosed(Ogre::RenderWindow* rw);
+	virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt);
+
 private:
 	Ogre::Root* mRoot;
 	Ogre::String mPluginsCfg;
 	Ogre::String mResourcesCfg;
+	Ogre::RenderWindow* mWindow;
+	Ogre::SceneManager* mSceneMgr;
+	Ogre::Camera* mCamera;
+
+	//OIS input devices
+	OIS::InputManager* mInputManager;
+	OIS::Mouse* mMouse;
+	OIS::Keyboard* mKeyboard;
 };
--- trunk/game/src/Game.cpp
+++ trunk/game/src/Game.cpp
@@ -2,6 +2,13 @@
 
 #include <OgreException.h>
 #include <OgreConfigFile.h>
+#include <OgreCamera.h>
+#include <OgreViewport.h>
+#include <OgreSceneManager.h>
+#include <OgreRenderWindow.h>
+#include <OgreEntity.h>
+
+#include <OgreWindowEventUtilities.h>
 
 Game::Game(void)
 	: mRoot(0),
@@ -12,25 +19,149 @@ Game::Game(void)
 
 Game::~Game(void)
 {
+	Ogre::WindowEventUtilities::removeWindowEventListener(mWindow, this);
+	windowClosed(mWindow);
 	delete mRoot;
 }
 
 bool Game::go(void)
 {
-//#ifdef _DEBUG
-//	mPluginsCfg = "plugins_d.cfg";
-//	mResourcesCfg = "resources_d.cfg";
-//#else
-//	mPluginsCfg = "plugins.cfg";
-//	mResourcesCfg = "resources.cfg";
-//#endif
-//
-//	//Contruir Ogre::Root
-//	mRoot = new Ogre::Root(mPluginsCfg);
-//
-//	//set up resources
-//	// Load resource paths from config file
+#ifdef _DEBUG
+	mPluginsCfg = "plugins_d.cfg";
+	mResourcesCfg = "resources_d.cfg";
+#else
+	mPluginsCfg = "plugins.cfg";
+	mResourcesCfg = "resources.cfg";
+#endif
 
+	// Contruir Ogre::Root
+	mRoot = new Ogre::Root(mPluginsCfg);
+
+	// Set up resources
+	// Load resource paths from config file
+	Ogre::ConfigFile cf;
+	cf.load(mResourcesCfg);
+
+	// Go through all sections & settings in the file
+	Ogre::ConfigFile::SectionIterator seci = cf.getSectionIterator();
+
+	Ogre::String secName, typeName, archName;
+	while (seci.hasMoreElements())
+	{
+		secName = seci.peekNextKey();
+		Ogre::ConfigFile::SettingsMultiMap *settings = seci.getNext();
+		Ogre::ConfigFile::SettingsMultiMap::iterator i;
+		for(i = settings->begin(); i != settings->end(); ++i)
+		{
+			typeName = i->first;
+			archName = i->second;
+			Ogre::ResourceGroupManager::getSingleton().addResourceLocation(archName, typeName, secName);
+		}
+	}
+
+	// configure
+	// Show the configuration dialog and initialise the system
+	if(!(mRoot->restoreConfig() || mRoot->showConfigDialog()))
+	{
+		return false;
+	}
+
+	mWindow = mRoot->initialise(true, "Game");
+
+	// Set default mipmap level
+	Ogre::TextureManager::getSingleton().setDefaultNumMipmaps(5);
+	// Initialise all resource groups
+	Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
+
+	// Create the SceneManager
+	mSceneMgr = mRoot->createSceneManager("DefaultSceneManager");
+
+	//Create the camera
+	mCamera = mSceneMgr->createCamera("PlayerCam");
+
+	mCamera->setPosition(Ogre::Vector3(0,0,80));
+
+	mCamera->lookAt(Ogre::Vector3(0,0,-300));
+	mCamera->setNearClipDistance(5);
+
+	//Creating viewport
+	Ogre::Viewport* vp = mWindow->addViewport(mCamera);
+	vp->setBackgroundColour(Ogre::ColourValue(0,0,0));
+
+	mCamera->setAspectRatio(Ogre::Real(vp->getActualWidth())/Ogre::Real(vp->getActualHeight()));
+
+	Ogre::Entity* ogreHead = mSceneMgr->createEntity("Head", "ogrehead.mesh");
+
+	Ogre::SceneNode* headNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+	headNode->attachObject(ogreHead);
+
+	//Set ambient light
+	mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5,0.5,0.5));
+
+	//Create a light
+	Ogre::Light* l = mSceneMgr->createLight("MainLight");
+	l->setPosition(20,80,50);
+
+	Ogre::LogManager::getSingletonPtr()->logMessage("*** Initializing OIS ***");
+	OIS::ParamList pl;
+	size_t windowHnd = 0;
+	std::ostringstream windowHndStr;
+
+	mWindow->getCustomAttribute("WINDOW", &windowHnd);
+	windowHndStr << windowHnd;
+	pl.insert(std::make_pair(std::string("WINDOW"), windowHndStr.str()));
+
+	mInputManager = OIS::InputManager::createInputSystem( pl );
+
+	mKeyboard = static_cast<OIS::Keyboard*>(mInputManager->createInputObject(OIS::OISKeyboard, false));
+	mMouse = static_cast<OIS::Mouse*>(mInputManager->createInputObject( OIS::OISMouse, false ));
+
+	windowResized(mWindow);
+
+	Ogre::WindowEventUtilities::addWindowEventListener(mWindow, this);
+
+	mRoot->addFrameListener(this);
+	mRoot->startRendering();
+
+	return true;
+}
+
+void Game::windowResized(Ogre::RenderWindow* rw)
+{
+	unsigned int width, height, depth;
+	int left, top;
+	rw->getMetrics(width, height, depth, left, top);
+
+	const OIS::MouseState &ms = mMouse->getMouseState();
+	ms.width = width;
+	ms.height = height;
+}
+
+void Game::windowClosed(Ogre::RenderWindow* rw)
+{
+	if(rw == mWindow)
+	{
+		if(mInputManager)
+		{
+			mInputManager->destroyInputObject(mMouse);
+			mInputManager->destroyInputObject(mKeyboard);
+
+			OIS::InputManager::destroyInputSystem(mInputManager);
+			mInputManager = 0;
+		}
+	}
+}
+
+bool Game::frameRenderingQueued(const Ogre::FrameEvent &evt)
+{
+	if(mWindow->isClosed())
+		return false;
+
+	mKeyboard->capture();
+	mMouse->capture();
+
+	if(mKeyboard->isKeyDown(OIS::KC_ESCAPE))
+		return false;
 	return true;
 }
 
